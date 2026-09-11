7dd1bdeb74e8815edefc8cdcc71269e969146d8d
Carsten Bach <mail@carsten-bach.de>
2026-09-11 13:26:47 +0200

    Place the multisite-list-block into the regular footer

--- wp-content/themes/jr26-experiments/patterns/footer.php
+++ wp-content/themes/jr26-experiments/patterns/footer.php
@@ -45,6 +45,16 @@
 <div style="height:var(--wp--preset--spacing--70)" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+<!-- /wp:separator -->
+
+<!-- wp:telex/block-telex-multisite-block {"orderBy":"registered","publicOnly":true,"direction":"horizontal","showDomain":false,"excludeCurrentSite":true,"className":"is-style-minimal","fontSize":"small","layout":{"type":"default"}} /-->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator has-alpha-channel-opacity"/>
+<!-- /wp:separator -->
+
 <!-- wp:group {"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignfull"><!-- wp:paragraph {"fontSize":"small"} -->
 <p class="has-small-font-size"><?php esc_html_e('jr26-experiments', 'jr26-experiments');?></p>
